40cf33005cefce8301e85bf240a99e8110859878
Chloe Wakefield <chloe.wakefield@bbc.co.uk>
2020-05-26 21:26:58 +0100

    wip

--- src/canvas.js
+++ src/canvas.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { v4 } from 'uuid';
 
 class Canvas extends Component {
   constructor(props) {
@@ -10,13 +9,10 @@ class Canvas extends Component {
   }
 
   isPainting = false;
-  // Different stroke styles to be used for user and guest
   userStrokeStyle = '#EE92C2';
   guestStrokeStyle = '#F0C987';
   line = [];
-  // v4 creates a unique id for each user. We used this since there's no auth to tell users apart
-  userId = v4();
-  prevPos = { offsetX: 0, offsetY: 0 };
+
 
   onMouseDown({ nativeEvent }) {
     const { offsetX, offsetY } = nativeEvent;
@@ -28,12 +24,10 @@ class Canvas extends Component {
     if (this.isPainting) {
       const { offsetX, offsetY } = nativeEvent;
       const offSetData = { offsetX, offsetY };
-      // Set the start and stop position of the paint event.
       const positionData = {
         start: { ...this.prevPos },
         stop: { ...offSetData },
       };
-      // Add the position to the line array
       this.line = this.line.concat(positionData);
       this.paint(this.prevPos, offSetData, this.userStrokeStyle);
     }
@@ -41,7 +35,6 @@ class Canvas extends Component {
   endPaintEvent() {
     if (this.isPainting) {
       this.isPainting = false;
-      this.sendPaintData();
     }
   }
   paint(prevPos, currPos, strokeStyle) {
@@ -50,36 +43,15 @@ class Canvas extends Component {
 
     this.ctx.beginPath();
     this.ctx.strokeStyle = strokeStyle;
-    // Move the the prevPosition of the mouse
     this.ctx.moveTo(x, y);
-    // Draw a line to the current position of the mouse
     this.ctx.lineTo(offsetX, offsetY);
-    // Visualize the line using the strokeStyle
     this.ctx.stroke();
     this.prevPos = { offsetX, offsetY };
   }
 
-  async sendPaintData() {
-    const body = {
-      line: this.line,
-      userId: this.userId,
-    };
-    // We use the native fetch API to make requests to the server
-    const req = await fetch('http://localhost:4000/paint', {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
-    const res = await req.json();
-    this.line = [];
-  }
-
   componentDidMount() {
-    // Here we set up the properties of the canvas element. 
-    this.canvas.width = 1000;
-    this.canvas.height = 800;
+    this.canvas.width=window.innerWidth
+    this.canvas.height=window.innerHeight
     this.ctx = this.canvas.getContext('2d');
     this.ctx.lineJoin = 'round';
     this.ctx.lineCap = 'round';
@@ -89,7 +61,6 @@ class Canvas extends Component {
   render() {
     return (
       <canvas
-      // We use the ref attribute to get direct access to the canvas element. 
         ref={(ref) => (this.canvas = ref)}
         style={{ background: 'black' }}
         onMouseDown={this.onMouseDown}
