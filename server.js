9a7eed5189d1ce83695b5915008056b372f7ea0a
Chloe Wakefield <chloe.wakefield@bbc.co.uk>
2020-05-26 21:16:02 +0100

    Wip

---
+++ server.js
@@ -0,0 +1,23 @@
+const io = require('socket.io')();
+
+function randomColour() {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected.`);
+  io.emit('chloe', '#'+randomColour());
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+    io.emit('wakefield', '#'+randomColour());
+  });
+
+  socket.on('test', (clientId) => {
+    console.log('received from client: ', clientId);
+  });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Canvas from './canvas';
+
+
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
+
+const NUM_POLL_SKIPS = 9;
+
+
 
 function App() {
+
+
+
+  const [colour, setColour] = useState('000000');
+
+  function printMouseCoords(e) {
+    console.log(
+      {
+        colour: colour,
+        x: e.clientX,
+        y: e.clientY,
+        mousedown: mousedown
+      });
+  }
+
+  document.onclick = (e) => {
+    //printMouseCoords(e);
+    //alert('my colour is ' + colour);
+    socket.emit('test', socket.id);
+  }
+
+
+  var mousedown = false;
+  document.onmousedown = (e) => {
+    mousedown = true;
+    printMouseCoords(e);
+  }
+  document.onmouseup = (e) => {
+    mousedown = false;
+    printMouseCoords(e);
+  }
+  var counter = 0;
+  document.onmousemove = (e) => {
+    if (mousedown) {
+      if (counter > NUM_POLL_SKIPS) {
+        counter = 0;
+        printMouseCoords(e);
+      }
+      counter++;
+    }
+  }
+
+  socket.on('connect', () => {
+    console.log('client, connect');
+    socket.on('chloe', (colour) => {
+      console.log('chloe');
+      document.getElementsByTagName('body')[0].style.backgroundColor = colour;
+      console.log(colour);
+      setColour(colour);
+    });
+    socket.on('wakefield', (colour) => {
+      console.log('wakefield');
+      document.getElementsByTagName('body')[0].style.backgroundColor = colour;
+      setColour(colour);
+    });
+  });
+
+
+
   return (
     <>
-      <h1>Working!</h1>
+      <Canvas />
     </>
   );
 }
